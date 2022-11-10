e5352e37e6e7a527f05c92068cc544a413824e56
Adinterm Prador <justwhiteraven@gmail.com>
2022-11-10 22:55:31 +0700

    Update rename_images.py

--- rename_images.py
+++ rename_images.py
@@ -1,21 +1,28 @@
 from pytesseract import image_to_string
 import pytesseract
 import PIL.Image
+import glob
 import cv2
+import os
 
+os.chdir() #choose the directory here
+format_file = 'JPG'
+files = sorted(glob.glob('*.{}'.format(format_file)
+               
 #Linux
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 #for windows user, try to find the file inside the program files directory
 
-'''<input_image> add later'''
-
-image = cv2.imread(input_image)
+#the position of the text you want to get (in pixel)
 y = #value
 x = #value
 height = #value
 width = #value
 
-crop_img = image[y:y+h, x:x+w]
-
-output = pytesseract.image_to_string(crop, lang='eng')
-print(output)
+for i in range(0,len(files)):
+  image = cv2.imread(files[i])
+  crop_img = image[y:y+height, x:x+width]
+  output = pytesseract.image_to_string(crop, lang='eng')
+  os.rename(files[i], "{}.{}".format(output, format_file))
+               
+print(files)
