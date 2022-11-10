b6dec3529a819dab185638fe4314e39bee252d30
Adinterm Prador <justwhiteraven@gmail.com>
2022-11-10 22:27:32 +0700

    Update rename_images.py

--- rename_images.py
+++ rename_images.py
@@ -2,3 +2,20 @@ from pytesseract import image_to_string
 import pytesseract
 import PIL.Image
 import cv2
+
+#Linux
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+#for windows user, try to find the file inside the program files directory
+
+'''<input_image> add later'''
+
+image = cv2.imread(input_image)
+y = #value
+x = #value
+height = #value
+width = #value
+
+crop_img = image[y:y+h, x:x+w]
+
+output = pytesseract.image_to_string(crop, lang='eng')
+print(output)
